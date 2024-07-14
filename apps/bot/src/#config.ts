cddce507e63e59ad3f1585f3bc5ef1de7626015d
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-07-14 15:08:56 -0400

    chore: add more roles and channel IDs

--- apps/bot/src/#config.ts
+++ apps/bot/src/#config.ts
@@ -1,8 +1,16 @@
 export default {
-    guildId: "780176091989803048",
+  guildId: "780176091989803048",
 
-    roleIds: {
-        governingBoard: "1232448282186350663",
-        coordinator: "1232449455580778506",
-    }
-}
+  roleIds: {
+    governingBoard: "1232448282186350663",
+    coordinator: "1232449455580778506",
+
+    busDriver: "1232447398165348433",
+    studentDriver: "1232447400291991603",
+  },
+
+  channels: {
+    routes: "860897795749707856",
+    trainings: "860897811914686475",
+  },
+};
