3b1d911632ab2b703bee14ab00b718bcf1338f3d
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-07-16 13:49:43 -0400

    feat: rank commands!

--- apps/bot/src/commands/rank.ts
+++ apps/bot/src/commands/rank.ts
@@ -3,6 +3,17 @@ import Config from "../#config";
 import noblox from "noblox.js";
 import bloxlinkGuild from "@codiium/bloxlink-api/guild";
 import embed from "../resources/templates/embed";
+import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  EmbedBuilder,
+  Guild,
+  TextChannel,
+  type User,
+} from "discord.js";
+import colors from "../resources/constants/colors";
+import { rank } from "../resources/modules/roblox";
 
 export class RankCommand extends Subcommand {
   public constructor(context: Subcommand.LoaderContext, options: Subcommand.Options) {
@@ -47,7 +58,7 @@ export class RankCommand extends Subcommand {
                     Object.keys(Config.groupRanks).map((rank) => {
                       return {
                         name: Config.groupRanks[rank as keyof typeof Config.groupRanks].friendlyName,
-                        value: Config.groupRanks[rank as keyof typeof Config.groupRanks].roleId,
+                        value: Config.groupRanks[rank as keyof typeof Config.groupRanks].rankId,
                       };
                     }),
                   )
@@ -156,18 +167,131 @@ export class RankCommand extends Subcommand {
     }
   }
 
+  private async logRankChange(
+    guild: Guild,
+    executor: User,
+    userId: number,
+    rankId: number,
+    reason: string,
+    request = true,
+  ) {
+    const { client } = this.container;
+
+    const logEmbed = new EmbedBuilder()
+      .setColor(colors.Blank)
+      .setDescription(
+        `## Rank ${request ? "Request" : "Change"} Log${request ? `\n-# Requests can only be approved by <@&${Config.roleIds.associateSupervisor}> or above` : ""}`,
+      )
+      .addFields(
+        {
+          name: ":bust_in_silhouette: Roblox User",
+          value: `[${await noblox.getUsernameFromId(userId)}](https://roblox.com/users/${userId})`,
+        },
+        {
+          name: "Rank",
+          value: `${(await noblox.getRole(Config.groupId, rankId)).name}`,
+        },
+        {
+          name: ":pencil: Reason",
+          value: reason,
+        },
+      )
+      .setAuthor({
+        name: `@${executor.username}`,
+        iconURL: executor.displayAvatarURL(),
+      })
+      // biome-ignore lint/style/noNonNullAssertion: client exists
+      .setThumbnail(client.user!.displayAvatarURL())
+      .setFooter({
+        // biome-ignore lint/style/noNonNullAssertion: client exists
+        text: client.user!.username,
+        // biome-ignore lint/style/noNonNullAssertion: client exists
+        iconURL: client.user!.displayAvatarURL(),
+      })
+      .setTimestamp();
+
+    const approveButton = new ButtonBuilder()
+      .setStyle(ButtonStyle.Primary)
+      .setLabel("Approve")
+      .setCustomId(`${userId}:${rankId}@rank.approve`);
+    const denyButton = new ButtonBuilder()
+      .setStyle(ButtonStyle.Danger)
+      .setLabel("Deny")
+      .setCustomId(`${userId}:${rankId}@rank.deny`);
+
+    if (!request) {
+      approveButton.setLabel("Approved").setDisabled(true);
+
+      denyButton.setStyle(ButtonStyle.Secondary).setDisabled(true);
+
+      rank(userId, rankId);
+    }
+
+    const buttonsRow = new ActionRowBuilder<ButtonBuilder>().addComponents(approveButton, denyButton);
+
+    const rankChangesChannel = (await guild.channels.fetch(Config.channels.rankChanges)) as TextChannel;
+
+    rankChangesChannel.send({
+      embeds: [logEmbed],
+      components: [buttonsRow],
+    });
+  }
+
   public async rankSet(interaction: Subcommand.ChatInputCommandInteraction) {
-    const robloxId = this.getRobloxUserIdFromInteraction(interaction);
+    const robloxId = await this.getRobloxUserIdFromInteraction(interaction);
     if (!robloxId) return;
+
+    const rank = interaction.options.getInteger("rank", true);
+    const reason = interaction.options.getString("reason", true);
+
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    await this.logRankChange(interaction.guild!, interaction.user, robloxId, rank, reason, false);
+
+    return interaction.reply({
+      embeds: [embed.information("Rank successfully set")],
+      ephemeral: true,
+    });
   }
 
   public async rankSuspend(interaction: Subcommand.ChatInputCommandInteraction) {
-    const robloxId = this.getRobloxUserIdFromInteraction(interaction);
+    const robloxId = await this.getRobloxUserIdFromInteraction(interaction);
     if (!robloxId) return;
+
+    const reason = interaction.options.getString("reason", true);
+
+    await this.logRankChange(
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      interaction.guild!,
+      interaction.user,
+      robloxId,
+      Config.groupRanks.administrativeLeave.rankId,
+      reason,
+    );
+
+    return interaction.reply({
+      embeds: [embed.information("Rank change successfully requested")],
+      ephemeral: true,
+    });
   }
 
   public async rankBusDriver(interaction: Subcommand.ChatInputCommandInteraction) {
-    const robloxId = this.getRobloxUserIdFromInteraction(interaction);
+    const robloxId = await this.getRobloxUserIdFromInteraction(interaction);
     if (!robloxId) return;
+
+    const reason = interaction.options.getString("reason", true);
+
+    await this.logRankChange(
+      // biome-ignore lint/style/noNonNullAssertion: <explanation>
+      interaction.guild!,
+      interaction.user,
+      robloxId,
+      Config.groupRanks.administrativeLeave.rankId,
+      reason,
+    );
+
+    return interaction.reply({
+      embeds: [embed.information("Rank change successfully requested")],
+      ephemeral: true,
+    });
   }
 }
