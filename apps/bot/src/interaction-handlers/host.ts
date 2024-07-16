5e3378df89f6195ede2f2c31dfdf3d1b90b4bfa1
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-07-16 11:37:01 -0400

    feat: finish host command

--- apps/bot/src/interaction-handlers/host.ts
+++ apps/bot/src/interaction-handlers/host.ts
@@ -9,6 +9,8 @@ import bloxlinkGuild from "@codiium/bloxlink-api/guild";
 import embed from "../resources/templates/embed";
 import noblox from "noblox.js";
 import Config from "../#config";
+import { lexer } from "marked";
+import { announceToGuilded, convertToGuilded } from "../resources/modules/convertToGuilded";
 
 export class OperationButtonHandler extends InteractionHandler {
   public constructor(ctx: InteractionHandler.LoaderContext, options: InteractionHandler.Options) {
@@ -59,10 +61,10 @@ export class OperationButtonHandler extends InteractionHandler {
         // biome-ignore lint/style/noNonNullAssertion: <explanation>
         interaction.message.embeds[0]!.fields[2]!.value.match(/(\d+)/)!;
       const time = new Date(0);
-      time.setSeconds(Number.parseInt(timeRegex[0]));
+      time.setSeconds(Number.parseInt(timeRegex[0]) + 3600);
 
       const timeLater = new Date(0);
-      timeLater.setSeconds(Number.parseInt(timeRegex[0]) + 5400);
+      timeLater.setSeconds(Number.parseInt(timeRegex[0]) + 9000);
 
       (await this.container.client.guilds.fetch(Config.guildId)).scheduledEvents.create({
         name: training ? "EBC CDL Training" : "Route: Redwater, Virginia",
@@ -86,6 +88,24 @@ export class OperationButtonHandler extends InteractionHandler {
         const bloxlinkUser = (await bloxlinkGuild.DiscordToRoblox(Config.guildId, discordId)).robloxID;
 
         const robloxUsername = await noblox.getUsernameFromId(Number.parseInt(bloxlinkUser));
+
+        const stringTime = `${time.toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "2-digit",
+        })} EST on ${time.toLocaleDateString(undefined, {
+          weekday: "long",
+          month: "short",
+          day: "numeric",
+        })}`;
+
+        const parsed = lexer(
+          training
+            ? `# CDL Training\nA CDL Training is being hosted by ${robloxUsername} at ${stringTime}\n\nYou can join the game using the link below:\nhttps://www.roblox.com/games/6817047049/EBC-CDL-Training-Center`
+            : `# Route\nA Route in Redwater, Virginia is being hosted by ${robloxUsername} at ${stringTime}\n\nYou can join the game using the link below:\nhttps://www.roblox.com/games/12254854680/Redwater-Virginia`,
+        );
+
+        const convertedToGuilded = convertToGuilded(parsed);
+        await announceToGuilded(convertedToGuilded);
       } catch (error) {
         console.log(error);
       }
