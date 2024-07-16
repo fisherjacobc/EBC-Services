a1d33c5fecf1710679db344423a9d1b9371df377
Fisher <72705185+fisherjacobc@users.noreply.github.com>
2024-07-16 11:46:10 -0400

    chore: add group ranks

--- apps/bot/src/#config.ts
+++ apps/bot/src/#config.ts
@@ -2,6 +2,65 @@ export default {
   guildId: "780176091989803048",
   groupId: 8406914,
 
+  groupRanks: {
+    seniorDeveloper: {
+      friendlyName: "Senior Developer",
+      roleId: 95,
+    },
+    developer: {
+      friendlyName: "Developer",
+      roleId: 85,
+    },
+    associateSupervisor: {
+      friendlyName: "Associate Supervisor",
+      roleId: 75,
+    },
+    transportationCoordinationAssociate: {
+      friendlyName: "Transportation Coordination Associate",
+      roleId: 70,
+    },
+    dispatchAndFleetAssuranceAssociate: {
+      friendlyName: "Dispatch & Fleet Assurance Associate",
+      roleId: 69,
+    },
+    moderationAssociate: {
+      friendlyName: "Moderation Associate",
+      roleId: 68,
+    },
+    trainingAssociate: {
+      friendlyName: "Training Associate",
+      roleId: 67,
+    },
+    associateTrainee: {
+      friendlyName: "Associate Trainee",
+      roleId: 62,
+    },
+    alliedAssociate: {
+      friendlyName: "Allied Associate",
+      roleId: 61,
+    },
+    honoraryAlumni: {
+      friendlyName: "Honorary Alumni",
+      roleId: 60,
+    },
+    leadDriver: {
+      friendlyName: "Lead Driver",
+      roleId: 58,
+    },
+    busDriver: {
+      friendlyName: "Bus Driver",
+      roleId: 54,
+    },
+    administrativeLeave: {
+      friendlyName: "Administrative Leave",
+      roleId: 52,
+    },
+    studentDriver: {
+      friendlyName: "Student Driver",
+      roleId: 50,
+    },
+  },
+
   roleIds: {
     governingBoard: "1232448282186350663",
     coordinator: "1232449455580778506",
